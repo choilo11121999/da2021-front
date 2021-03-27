36a61887c4ae3abcde40f1c6b77b5e25f5fd66d2
choilo11121999 <choilo11121999@gmail.com>
2021-03-27 16:42:13 +0700

    login #1

--- da/src/App.js
+++ da/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
-import { Login } from './components/login/login';
+import { Login } from './components/login/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App bg-light">
+        <div className="d-flex justify-content-center">
+          <div className="text-center">
+            <Link to="/login" className="btn btn-primary" data-toggle="modal" data-target="#signupPage">Sign in</Link>
+          </div>
+          <div className="text-center">
+            <a href="" className="btn btn-primary" data-toggle="modal" data-target="#signupPage">Sign up</a>
+          </div>
+        </div>
+      </div>
+      <Switch>
+        <Route path="/login" component={Login}/>
+        {/* <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route> */}
+      </Switch>
+    </Router>
   );
 }
 
