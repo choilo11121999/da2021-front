4439b5cf54cacc68d4f832dffbd1f26d64aba204
choilo11121999 <choilo11121999@gmail.com>
2021-03-27 16:42:13 +0700

    login #1

--- da/src/components/login/Login.js
+++ da/src/components/login/Login.js
@@ -1,34 +1,58 @@
 export const Login = () => {
   return (
-    <div className="container">
-      <form>
-        <div className="form-group">
-          <label for="email">Email address:</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            id="email"
-          />
+    <div className="modal fade" id="signinPage" style={{marginTop: '100px'}}>
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header text-center">
+            <h4 className="modal-title text-center w-100 font-weight-bold">
+              Sign in
+            </h4>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-lable="close"
+            >
+              &times;
+            </button>
+          </div>
+          <div className="modal-body">
+            <form action="/examples/actions/confirmation.php" method="post">
+              <div className="form-group">
+                <i className="fas fa-user"></i>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Username"
+                  required="required"
+                />
+              </div>
+              <div className="form-group">
+                <i className="fas fa-lock"></i>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  required="required"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-block btn-lg"
+                  value="Login"
+                />
+              </div>
+            </form>
+          </div>
+          <div className="modal-footer d-flex flex-column">
+            <div><a href="#">Forgot Password?</a></div>
+            <div>
+              Ban chua co tai khoan? <a href="facebook.com">Dang ky</a>
+            </div>
+          </div>
         </div>
-        <div className="form-group">
-          <label for="pwd">Password:</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            id="pwd"
-          />
-        </div>
-        <div className="form-group form-check">
-          <label className="form-check-label">
-            <input className="form-check-input" type="checkbox" /> Remember me
-          </label>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Login
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
