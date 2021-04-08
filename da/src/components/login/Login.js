bc1c3640b09d5238e38e5fdcfdec61d285f7e26b
choilo11121999 <choilo11121999@gmail.com>
2021-04-08 16:10:56 +0700

    home page #1

--- da/src/components/login/Login.js
+++ da/src/components/login/Login.js
@@ -1,56 +1,46 @@
 export const Login = () => {
   return (
-    <div className="modal fade" id="signinPage" style={{marginTop: '100px'}}>
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header text-center">
-            <h4 className="modal-title text-center w-100 font-weight-bold">
-              Sign in
-            </h4>
-            <button
-              type="button"
-              className="close"
-              data-dismiss="modal"
-              aria-lable="close"
-            >
-              &times;
-            </button>
+    <div className="login">
+      <div>
+        <h4 className="text-center w-100 font-weight-bold">
+          Sign in
+        </h4>
+      </div>
+      <div className="">
+        <form action="" method="post">
+          <div className="form-group">
+            <i className="fas fa-user"></i>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Username"
+              required="required"
+            />
           </div>
-          <div className="modal-body">
-            <form action="/examples/actions/confirmation.php" method="post">
-              <div className="form-group">
-                <i className="fas fa-user"></i>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Username"
-                  required="required"
-                />
-              </div>
-              <div className="form-group">
-                <i className="fas fa-lock"></i>
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Password"
-                  required="required"
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="submit"
-                  className="btn btn-primary btn-block btn-lg"
-                  value="Login"
-                />
-              </div>
-            </form>
+          <div className="form-group">
+            <i className="fas fa-lock"></i>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              required="required"
+            />
           </div>
-          <div className="modal-footer d-flex flex-column">
-            <div><a href="#">Forgot Password?</a></div>
-            <div>
-              Ban chua co tai khoan? <a href="facebook.com">Dang ky</a>
-            </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              className="btn btn-primary btn-block btn-lg"
+              value="Login"
+            />
           </div>
+        </form>
+      </div>
+      <div className="">
+        <div>
+          <a href="#">Forgot Password?</a>
+        </div>
+        <div>
+          Ban chua co tai khoan? <a href="facebook.com">Dang ky</a>
         </div>
       </div>
     </div>
