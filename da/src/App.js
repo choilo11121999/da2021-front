33283d145a874a1c7d519d4ed7727d177ee89282
choilo11121999 <57838904+choilo11121999@users.noreply.github.com>
2021-04-23 21:05:56 +0700

    Home page (#2)
    * login #1
    
    * home page #1
    
    * home-page

--- da/src/App.js
+++ da/src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
-import { Login } from './components/login/Login';
+import Login from './components/login/Login';
 import Register from './components/register/Register';
+import Header from './components/user/Header';
+import Footer from './components/user/Footer';
+import Home from './components/user/Home';
+import MoviePlaying from './components/user/MoviePlaying';
+import MovieUpcoming from './components/user/MovieUpcoming';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Login />
-      {/* <Register/> */}
+   <Router>
+    <div className="container class"> 
+      <Header />
+
+      {/* Router */}
+      <Route path="/" exact component={Home}/>
+      <Route path="/user/login" component={Login}/>
+      <Route path="/user/register" component={Register}/>
+      <Route path="/movie/movie_playing" component={MoviePlaying} />
+      <Route path="/movie/movie_upcoming" component={MovieUpcoming} />
     </div>
+
+    <Footer />
+  </Router>
   );
 }
 
--- da/src/components/login/Login.js
+++ da/src/components/login/Login.js
@@ -1,34 +1,53 @@
-export const Login = () => {
+import './../../css/Login.css';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
   return (
-    <div className="container">
-      <form>
-        <div className="form-group">
-          <label for="email">Email address:</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            id="email"
-          />
+    <div className="login">
+      <div className="login-header">
+        <h2 className="text-center w-100 font-weight-bold">
+          Đăng Nhập
+        </h2>
+      </div>
+      <div className="login-body my-3">
+        <form action="" method="post">
+          <div className="form-group my-4">
+            <i className="fas fa-user"></i>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email"
+              required="required"
+            />
+          </div>
+          <div className="form-group my-4">
+            <i className="fas fa-lock"></i>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Mật khẩu"
+              required="required"
+            />
+          </div>
+          <div className="form-group my-4">
+            <input
+              type="submit"
+              className="btn btn-danger btn-block btn-lg"
+              value="Đăng nhập"
+            />
+          </div>
+        </form>
+      </div>
+      <div className="login-footer">
+        <div>
+          <Link to="/user/forgot_password">Quên mật khẩu?</Link>
         </div>
-        <div className="form-group">
-          <label for="pwd">Password:</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            id="pwd"
-          />
+        <div>
+          Bạn chưa có tài khoản? <Link to="/user/register">Đăng ký</Link>
         </div>
-        <div className="form-group form-check">
-          <label className="form-check-label">
-            <input className="form-check-input" type="checkbox" /> Remember me
-          </label>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Login
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
+
+export default Login;
